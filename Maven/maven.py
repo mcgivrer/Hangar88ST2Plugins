64466a4b91693666ec8657fa3e4c11ec715438d0
Frédéric Delorme <frederic.delorme@gmail.com>
2012-06-22 16:56:09 +0200

    start implementation of historic

--- Maven/maven.py
+++ Maven/maven.py
@@ -3,15 +3,17 @@ import sublime, sublime_plugin
 class MavenGoalsCommand(sublime_plugin.WindowCommand):
 	wrkDir = None
 	cmd = None
+	historic=[]
 
 	def run(self,working_dir,cmd):
 		self.cmd = [cmd]
 		self.wrkDir = working_dir
 		#print self.wrkDir
-		self.window.show_input_panel("mvn",'clean test',self.on_done,None,None)
+		self.window.show_input_panel("mvn",'',self.on_done,None,None)
 
 	def on_done(self, text):
 		self.cmd += [u'-B']
 		self.cmd += text.split(' ')
 		#print cmd
+		self.historic.append(self.cmd)
 		self.window.run_command("exec",{"cmd":self.cmd, 'working_dir':self.wrkDir})
